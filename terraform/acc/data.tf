9c2e4320a54628876cff3dffe1e3d5445bc52bf2
Jodasc <caio.jodas@healthalliance.co.nz>
2023-04-17 17:21:28 +1200

    create - SQMLI

--- terraform/acc/data.tf
+++ terraform/acc/data.tf
@@ -4,10 +4,10 @@ data "azurerm_private_dns_zone" "dns_zone" {
   resource_group_name = "sha-infra-dns-rg-ae-1"
 }
 
-# data "azuread_group" "sql_admin_group" {
-#   display_name = "Azure_SEC_Role_Project_Centric_Full"
-#   security_enabled = true
-# }
+data "azuread_group" "sql_admin_group" {
+  display_name = "Azure_SEC_Role_Project_Centric_Full"
+  security_enabled = true
+}
  
  data "azurerm_key_vault_secret" "acc_cert_password" {
   name         = "azure-centric-acc-hanz-health-nz"
