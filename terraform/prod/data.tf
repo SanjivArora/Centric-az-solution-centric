9bc11196b692ca92f44c3181ecc2f6aaddbf0257
Sharan Nagulapalli (healthAlliance) <Sharan.Nagulapalli@healthalliance.co.nz>
2023-05-22 14:24:41 +1200

    fix: uncomment data

--- terraform/prod/data.tf
+++ terraform/prod/data.tf
@@ -15,7 +15,7 @@ data "azuread_group" "sql_admin_group" {
   security_enabled = true
 }
  
-/*  data "azurerm_key_vault_secret" "prod_cert_password" {
+data "azurerm_key_vault_secret" "prod_cert_password" {
   name         = "azure-centric-prod-hanz-health-nz"
   key_vault_id = "/subscriptions/9e964e2d-61fe-4b12-83ed-297941d9d161/resourceGroups/prod-centric-tf-rg-ae-1/providers/Microsoft.KeyVault/vaults/prodcentrictfkvae1"
-} */
+}
